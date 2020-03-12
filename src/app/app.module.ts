6042d0cd989af7ab03036676e6e5455a52438b57
Prakash G <Prakash.G@Ltts.com>
2020-03-12 19:46:02 +0530

    Designed as per the mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HighchartsChartModule } from 'highcharts-angular';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HealthOverviewComponent } from './health-overview/health-overview.component';
 
 @NgModule({
@@ -15,7 +17,11 @@ import { HealthOverviewComponent } from './health-overview/health-overview.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HighchartsChartModule
+    HighchartsChartModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/health-overview/health-overview.component.ts
+++ src/app/health-overview/health-overview.component.ts
@@ -7,58 +7,115 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./health-overview.component.css']
 })
 export class HealthOverviewComponent implements OnInit {
+  healthTitle = 'Health Overview';
 
   constructor() { }
   highcharts = Highcharts;
+ 
   chartOptions = {   
      chart : {
+      backgroundColor: null,
         plotBorderWidth: null,
+        plotBackgroundColor: null,
         plotShadow: false,
-        height: 300,
-           width: 256
+        height: 400,
+        width: 390
      },
      title : {
-        text: 'Health Overview'   
+        text: '150',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 20,
+        x:-55,
+        style: {
+          fontWeight: 'bold',
+          color: '#1F1F1F',
+          fontSize: "44px",
+          letterSpacing: "-3px"
+    },   
      },
+     subtitle: {
+      text: 'Total assets',
+      align: 'center',
+        verticalAlign: 'middle',
+        y: 25,
+        x:-45,
+        style: {
+          color: '#1F1F1F',
+          fontSize: "14px"
+    },
+  },
+     credits: {
+      enabled: false
+  },
      tooltip : {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
      },
      plotOptions : {
        pie: {
          dataLabels: {
-             enabled: true,
+             enabled: false,
              distance: -100,
              style: {
                  fontWeight: 'bold',
                  color: 'red'
            },
-           showInLegend: true
+           
          },
          startAngle: -360,
          endAngle: 360,
-         center: ['53%', '45%'],
+         center: ['48%', '45%'],
          size: '110%'
      }
      },
      legend: {
-        width: 200,
-        layout: 'vertical',
-        useHTML: true,
-        labelFormatter: function () {
-            return "<div style='width:200px;'><span style='float:left;'>" + this.name + "</span><span style='float:right;margin-right: 30px;'>" + this.y + " kWh</span></div>";
-        }
-    },
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'top',
+      y: 100,
+      x: 20,
+      symbolHeight: 8,
+      symbolWidth: 8,
+      symbolRadius: 0
+  },
      series : [{
-        type: 'pie',
+      colorByPoint: true,
+      data: [{
+        name: '120 Active',
+        y: 86,
+        color: '#0CA919',
+      }, {
+        name: '3 Warnings',
+        y: 8,
+        color: '#FF7300',
+      }, {
+        name: '1 Critical',
+        y: 1,
+        color: '#F03040',
+      }, {
+        name: '26 Inactive',
+        y: 5,
+        color: '#DBDBDB',
+      }],
+      type: 'pie',
        name: 'Browser share',
-       innerSize: '86%',
-        data: [
-         ['Chrome', 88],
-         ['Firefox', 4],
-         ['Internet Explorer', 4],
-         ['Edge', 4],
-        ]
-     }]
+       innerSize: '84%', 
+      showInLegend: true,
+     }],
+     responsive: {
+      rules: [{
+          condition: {
+              maxWidth: 250
+          },
+          chartOptions: {
+              legend: {
+                  align: 'center',
+                  verticalAlign: 'bottom',
+                  layout: 'horizontal'
+              }
+          }
+      }]
+  }
   };
   ngOnInit(): void {
   }
